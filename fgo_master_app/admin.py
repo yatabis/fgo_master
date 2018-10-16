7b4541b9050e82f4752318243a9283a0ae8c58f0
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-10-16 23:39:27 +0900

    update: adminページの設定

--- fgo_master_app/admin.py
+++ fgo_master_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Servant
+from .models import Servant, NoblePhantasm, ActiveSkill, PassiveSkill, Synthesis
 
 # Register your models here.
 
 
 admin.site.register(Servant)
+admin.site.register(NoblePhantasm)
+admin.site.register(ActiveSkill)
+admin.site.register(PassiveSkill)
+admin.site.register(Synthesis)
