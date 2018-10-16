ef720de756b0bd7d996eacab1efb61015ac4420f
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-10-16 15:52:09 +0900

    update: 霊基一覧ページにリンクを貼った

--- fgo_master_app/urls.py
+++ fgo_master_app/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 
 from .views import TopMenuView
 from .views import CardDataView, SynthesisSchemeView, BattleSimulatorView
+from .views import ServantView, CraftEssenceView, CommandCodeView, MysticCodeView
 
 urlpatterns = [
     path('top-menu/', TopMenuView.as_view(), name="top-menu"),
+
     path('card-data/', CardDataView.as_view(), name="card-data"),
     path('synthesis-scheme/', SynthesisSchemeView.as_view(), name="synthesis-scheme"),
-    path('battle-simulator', BattleSimulatorView.as_view(), name="battle-simulator")
+    path('battle-simulator', BattleSimulatorView.as_view(), name="battle-simulator"),
+
+    path('servant/', ServantView.as_view(), name="servant"),
+    path('craft-essence/', CraftEssenceView.as_view(), name="craft-essence"),
+    path('command-code/', CommandCodeView.as_view(), name="command-code"),
+    path('mystic-code/', MysticCodeView.as_view(), name="mystic-code"),
 ]
--- fgo_master_app/views.py
+++ fgo_master_app/views.py
@@ -4,10 +4,12 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 
+# ルート階層
 class TopMenuView(TemplateView):
     template_name = "fgo_master_app/top-menu.html"
 
 
+# TopMenu階層
 class CardDataView(TemplateView):
     template_name = "fgo_master_app/card-data.html"
 
@@ -18,3 +20,20 @@ class SynthesisSchemeView(TemplateView):
 
 class BattleSimulatorView(TemplateView):
     template_name = "fgo_master_app/battle-simulation.html"
+
+
+# CardData階層
+class ServantView(TemplateView):
+    template_name = "fgo_master_app/servant.html"
+
+
+class CraftEssenceView(TemplateView):
+    template_name = "fgo_master_app/craft-essence.html"
+
+
+class CommandCodeView(TemplateView):
+    template_name = "fgo_master_app/command-code.html"
+
+
+class MysticCodeView(TemplateView):
+    template_name = "fgo_master_app/mystic-code.html"
